18ff1a8333eef756f7c7e8e152550eacbdaac36a
Jonathan V. Solórzano <jonathanvsv@gmail.com>
2023-09-12 13:33:29 -0600

    Added gitlab link

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -17,12 +17,6 @@ var store = [{
         "url": "/blog/manejo-imagenes/",
         "teaser": null
       },{
-        "title": "Working with LiDAR data in R",
-        "excerpt":"Working with LiDAR data in R For this example we are going to use the lidR package. Additionally, we are going to load raster rgdal and plyr. The first thing is to load the LiDAR data as a catalog and load a shapefile with the areas of interest (that can...","categories": ["blog"],
-        "tags": ["post","r","LiDAR","point cloud","raster"],
-        "url": "/blog/working-with-lidar/",
-        "teaser": null
-      },{
         "title": "Calculating image texture in R",
         "excerpt":"Image texture in R In this post I will show you how to calculate image texture in R. These textures have been used to model diversity and structural attributes of different forests with intermediate to very high R^2 values. Image textures are metrics that summarise the pixel’s tone variability in...","categories": ["blog"],
         "tags": ["post","r","texture","glcm","foto"],
@@ -107,19 +101,13 @@ var store = [{
         "url": "/blog/landscape-metrics-in-r/",
         "teaser": null
       },{
-        "title": "Species-occurrence-data-in-r",
+        "title": "Species occurrence data in r",
         "excerpt":"Species occurrence data The purpose of this post is to download data from GBIF to obtain occurrences registries of a particular species and then to transform that data into a geospatial object and obtain a map. The first step is to load the required packages. Rgbif is the package that...","categories": ["blog"],
         "tags": ["post","r","rgbif","gbif","biogeogrpahy","species occurrence"],
         "url": "/blog/species-occurrence-data-in-r/",
         "teaser": null
       },{
-        "title": "Beautiful-plots(ggplot2)-in-r",
-        "excerpt":"Beautiful plots in R using ggplot2 The purpose of this post is to show how to use the basic syntax of ggplot2, do some of the most common types of plots, show some customizations and facets. library(rgbif) library(skimr) library(dplyr) library(tidyr) library(rnaturalearth) library(sf) library(tmap) The first step will be to set...","categories": ["blog"],
-        "tags": ["post","r","ggplot2","plot","grammar of graphics","figures"],
-        "url": "/blog/beautiful-plots(ggplot2)-in-r/",
-        "teaser": null
-      },{
-        "title": "3D-maps-in-r",
+        "title": "3D maps in r",
         "excerpt":"3D maps in R The purpose of this post is to show how make a 3D map using rayshader. The idea is to use a DEM obtained from the SRTM mission (worldwide cover) or an available DEM from other National Institutions, such as Mexico’s National Institute of Statistics and Geography...","categories": ["blog"],
         "tags": ["post","r","rayshader","3d relief","3d dem","DEM","Relief"],
         "url": "/blog/3d-maps-in-r/",
@@ -141,5 +129,53 @@ var store = [{
         "excerpt":"Volcanos 3d maps I have continued playing with rayshader, rayvista and magick packages to make beautiful 3d maps. This post shows the code used to make a 3d map with some of the highest peaks in Mexico, which all have a volcanic origin. Additionally, the area shown in the map...","categories": ["blog"],
         "tags": ["post","r","rayshader","rayvista","magick","terra","3d maps","RGB shaded relief","DEM","Relief"],
         "url": "/blog/Volcanos-rayshader/",
+        "teaser": null
+      },{
+        "title": "Spotify API in R",
+        "excerpt":"Spotify API in R This post will show you how to connect to the Spotify API using R and the spotifyR package. This API enables you to extract data for particular artists or songs from the Spotify database. The first step is registering in the Spotify Developer App: https://developer.spotify.com/. Once...","categories": ["blog"],
+        "tags": ["post","r","spotify","music","API"],
+        "url": "/blog/spotify-in-r/",
+        "teaser": null
+      },{
+        "title": "Shiny App with spatial data",
+        "excerpt":"Shiny App with spatial data This post will show you how to build a shiny app to visualize spatial data (mainly in raster and vector format). Remember that you can also build other types of shiny apps, but I decided to focus this post on spatial data. So the first...","categories": ["blog"],
+        "tags": ["post","r","shiny","app","spatial","visualization","webapp"],
+        "url": "/blog/shiny-app-spatial/",
+        "teaser": null
+      },{
+        "title": "Web scraping with r",
+        "excerpt":"Web scraping with R This post will show you how to get data from a webpage (also known as web scraping) with R and the rvest package. This analysis was performed to complement the data obtained from the spotify API. Since I could not obtain all the data I was...","categories": ["blog"],
+        "tags": ["post","r","rvest","web scraping","website data extraction"],
+        "url": "/blog/web-scraping-with-r/",
+        "teaser": null
+      },{
+        "title": "Open Foris tools in r",
+        "excerpt":"Open Foris tools in R In this post I will show you how to easily run two Open Foris tools in R to easily validate a map and obtain the area estimates corrected by the producer’s accuracies obtained in the confusion matrix. The repository of these tools can be found...","categories": ["blog"],
+        "tags": ["post","r","Open Foris","map","accuracy assessment","area estimates","map verification","map validation","overall accuracy"],
+        "url": "/blog/open-foris-tools/",
+        "teaser": null
+      },{
+        "title": "Beautiful-plots(ggplot2)-in-r",
+        "excerpt":"Beautiful plots in R using ggplot2 The purpose of this post is to show how to use the basic syntax of ggplot2, do some of the most common types of plots, as well as some customizations and facets. For this post we are going to use the iris dataset, as...","categories": ["blog"],
+        "tags": ["post","r","ggplot2","plot","grammar of graphics","figures"],
+        "url": "/blog/beautiful-plots(ggplot2)-in-r/",
+        "teaser": null
+      },{
+        "title": "Working with LiDAR data in R",
+        "excerpt":"Working with LiDAR data in R For this example we are going to use the lidR package. Additionally, we are going to load terra sf and dplyr. In this example, we will work with lascatalogs, which enables working with several las files at once. The first thing is to load...","categories": ["blog"],
+        "tags": ["post","r","LiDAR","point cloud","raster","lidR"],
+        "url": "/blog/working-with-lidar/",
+        "teaser": null
+      },{
+        "title": "Rasters and vectors with terra",
+        "excerpt":"Rasters and vectors with terra This post shows a simple example of how to work with rasters and vectors using the terra package. Terra replaces the older raster package, since terra is usually faster to use. library(tibble) library(terra) library(dplyr) Then create some objects to work with and plot them. im1...","categories": ["blog"],
+        "tags": ["post","r","ggplot2","plot","grammar of graphics","figures"],
+        "url": "/blog/rasters-and-vectors-with-terra/",
+        "teaser": null
+      },{
+        "title": "AGB forest sampling calculations",
+        "excerpt":"AGB forest sampling calculations This post shows an example of how to calculate some common plot-level variables from a forest sampling. library(BIOMASS) library(stringr) library(tidyverse) Read data # Field data with individual tree measures df &lt;- read.csv(\"D:/Drive/Jonathan_trabaggio/Doctorado/R/Ayuquila_Degradation/CleanData/df_all.csv\", na.strings = \"NA\") # Coordinates of each site. coords &lt;- read.csv(\"D:/Drive/Jonathan_trabaggio/Doctorado/R/Ayuquila_Degradation/Data/gpscoords.csv\") How the headers...","categories": ["blog"],
+        "tags": ["post","r","BiOMASS","AGB","community attributes","plot-level calculations"],
+        "url": "/blog/AGB-forest-sampling-calculations/",
         "teaser": null
       }]
